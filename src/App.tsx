c1a5c8ffe5bfa16da1147b2ddf2f701a6de24eb1
Silvino <gino_sil@hotmail.com>
2021-05-07 22:45:04 -0500

    [FEATURE][FXFE-0002]: The button component has been modified #2

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ButtonDemo } from "./components/Atoms/Button/Button"
 
 function App() {
   return (
-    <ButtonDemo label='Hola Mundo desde typescript react' />
+    <ButtonDemo label='Hola Mundo desde typescript react'  />
   )
 }
 
