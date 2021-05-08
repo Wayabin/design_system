abebce9a7d303d57f22dc5ad4da24cbe6c494404
Silvino <gino_sil@hotmail.com>
2021-05-07 22:18:34 -0500

    [FEATURE][FXFE]: Some changes into the project

--- src/components/Atoms/Button/__tests__/Button.tsx
+++ src/components/Atoms/Button/__tests__/Button.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { render } from "@testing-library/react";
-import {ButtonDemo} from "../Button";
+import React from "react"
+import { render } from "@testing-library/react"
+import { ButtonDemo } from "../Button"
 
-test('render AttendanceCalendar', () => {
+test("render AttendanceCalendar", () => {
     render(<ButtonDemo />)
 })
