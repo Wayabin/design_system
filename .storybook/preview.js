de96e52dc7c53ba6b5c682b220f1df213f0bf81e
Wayabin <38799290+Wayabin@users.noreply.github.com>
2021-05-13 23:24:23 -0500

    Feature/fxfe 0002 creacion de boton (#4)
    * [FEATURE][FXFE-0002]: The button component has been modified #2

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,55 @@
 
+import React from 'react';
+import './design.system.universe.scss';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+
+	viewport: {
+		viewports: {
+			
+			mobile: {
+				name: 'Mobile (540)',
+				styles: {
+					width: '540px',
+					height: '768px',
+				},
+				type: 'mobile',
+			},
+			
+			desktop: {
+				name: 'Desktop (1024)',
+				styles: {
+					width: '1024px',
+					height: '768px',
+				},
+				type: 'tablet',
+			}
+		},
+	},
+
+	options: {
+		// showRoots: true,
+		
+		storySort: (a, b) => {
+			// console.log(a)
+			// const result = itemA.id.localeCompare(itemB.id, undefined, { numeric: true });
+			// console.log(`${itemA.id} < ${itemB.id} ? ${result}`);
+			const itemA = a[1];
+			const itemB = b[1];
+			const paramsA = a[2];
+			const paramsB = b[2];
+
+			return itemA.kind === itemB.kind ? 0 : 
+				paramsA.position && paramsB.position ?
+					paramsA.position.localeCompare(paramsB.position, undefined, { numeric: true }) :
+					itemA.kind.localeCompare(itemB.kind, undefined, { numeric: true })
+		}
+	}
+};
+
+
+export const decorators = [
+	(Story) => (
+				<Story />
+	),
+];
