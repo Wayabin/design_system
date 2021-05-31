a7f34d5a9bb3a956ce173559d77ca477f27a12bc
Silvino <gino_sil@hotmail.com>
2021-05-31 15:53:37 -0500

    The baseline has ben configured

--- components/Atoms/Button/Button.stories.tsx
+++ components/Atoms/Button/Button.stories.tsx
@@ -4,7 +4,7 @@ import { Story, Meta } from '@storybook/react'
 import { CustomButton, CustomButtonProps } from './Button'
 
 export default {
-  title: 'Design System/Button',
+  title: 'Design System/Atoms/Button',
   component: CustomButton,
   argTypes: {
     backgroundColor: { control: 'color' },
