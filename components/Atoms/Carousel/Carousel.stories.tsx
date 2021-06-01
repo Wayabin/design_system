c41650edbf0c8e369b93fa379fac6ef01ab805cc
Silvino <gino_sil@hotmail.com>
2021-06-01 16:30:20 -0500

    conflicts solved

--- components/Atoms/Carousel/Carousel.stories.tsx
+++ components/Atoms/Carousel/Carousel.stories.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react"
 import { Story, Meta } from "@storybook/react"
 import {Banner} from '../Banner/Banner'
@@ -7,25 +6,12 @@ import { Carousel, CarouselProps } from "./Carousel"
 
 export default {
   title: "Design System/Atoms/Carousel",
-=======
-import React from 'react'
-import { Story, Meta } from '@storybook/react'
-
-import { Carousel, CarouselProps } from './Carousel'
-
-export default {
-  title: 'Design System/Atoms/Carousel',
->>>>>>> a14f013 (Feature/crear carousel (#5))
   component: Carousel,
   parameters: {
     position: "0.2",
   },
   argTypes: {
-<<<<<<< HEAD
     backgroundColor: { control: "color" },
-=======
-    backgroundColor: { control: 'color' },
->>>>>>> a14f013 (Feature/crear carousel (#5))
   },
 } as Meta
 
@@ -34,17 +20,7 @@ const Template: Story<CarouselProps> = (args) => <Carousel {...args} />
 export const CarouselDemo = Template.bind({})
 
 CarouselDemo.args = {
-<<<<<<< HEAD
   carouselItems: [<Banner/>, <Banner/>],
 }
 
 
-=======
-  primary: true,
-  label: 'Button',
-  onClick: () => {
-    alert('Primary')
-  }
-}
-
->>>>>>> a14f013 (Feature/crear carousel (#5))
