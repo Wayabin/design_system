17723e40be712632238b2f9d26dacf00f973dcd5
Wayabin <38799290+Wayabin@users.noreply.github.com>
2021-05-31 23:31:44 -0500

    Feature/crear carousel (#5)
    * [FEATURE][FXFE-0002]: The button component has been modified #1
    
    * [FEATURE][FXFE-0002]: The button component has been modified #2
    
    * [FEATURE][DEPENDECIES]: The dependencies has been updated
    
    * The baseline has been set
    
    * The baseline has ben configured
    
    * [FEATURE][FX0002]: Carousel component has been created

--- components/Atoms/Carousel/Carousel.stories.tsx
+++ components/Atoms/Carousel/Carousel.stories.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react"
 import { Story, Meta } from "@storybook/react"
 import {Banner} from '../Banner/Banner'
@@ -6,12 +7,25 @@ import { Carousel, CarouselProps } from "./Carousel"
 
 export default {
   title: "Design System/Atoms/Carousel",
+=======
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
+
+import { Carousel, CarouselProps } from './Carousel'
+
+export default {
+  title: 'Design System/Atoms/Carousel',
+>>>>>>> a14f013 (Feature/crear carousel (#5))
   component: Carousel,
   parameters: {
     position: "0.2",
   },
   argTypes: {
+<<<<<<< HEAD
     backgroundColor: { control: "color" },
+=======
+    backgroundColor: { control: 'color' },
+>>>>>>> a14f013 (Feature/crear carousel (#5))
   },
 } as Meta
 
@@ -20,7 +34,17 @@ const Template: Story<CarouselProps> = (args) => <Carousel {...args} />
 export const CarouselDemo = Template.bind({})
 
 CarouselDemo.args = {
+<<<<<<< HEAD
   carouselItems: [<Banner/>, <Banner/>],
 }
 
 
+=======
+  primary: true,
+  label: 'Button',
+  onClick: () => {
+    alert('Primary')
+  }
+}
+
+>>>>>>> a14f013 (Feature/crear carousel (#5))
