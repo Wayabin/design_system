46853285ae12c279944863e70881c8461c33a9b6
Silvino <gino_sil@hotmail.com>
2021-06-01 22:39:52 -0500

    Correcion de precio tecate

--- pages/store/index.tsx
+++ pages/store/index.tsx
@@ -19,7 +19,7 @@ const Products = [
     title: "Cerveza Tecate Lata 473 ml.",
     text:
       "Una cerveza lager dorada, brillante y bien equilibrada con un sabor crujiente a malta, un amargor bajo a medio que termina limpio. Originaria de hace 60 años en la cervecería en el pueblo de Tecate en Baja California (México), Tecate original fue su primera cerveza de autor.",
-    price: "21.00",
+    price: "18.00",
   },
   {
     img:
